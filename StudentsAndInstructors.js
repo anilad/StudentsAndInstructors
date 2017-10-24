977647d7820496bb21fb33f00834e3ba39d567ea
anilad <aniladdao@gmail.com>
2017-10-23 19:35:51 -0700

    made changes to StudentsAndInstructors.js

---
+++ StudentsAndInstructors.js
@@ -0,0 +1,29 @@
+var students = [
+    { first_name: 'Michael', last_name: 'Jordan' },
+    { first_name: 'John', last_name: 'Rosales' },
+    { first_name: 'Mark', last_name: 'Guillen' },
+    { first_name: 'KB', last_name: 'Tonel' }
+];
+
+function print(arr) {
+    for (var i in arr) {
+        console.log(arr[i].first_name, arr[i].last_name);
+    }
+}
+
+print(students);
+
+var users = {
+    'Students': [
+        { first_name: 'Michael', last_name: 'Jordan' },
+        { first_name: 'John', last_name: 'Rosales' },
+        { first_name: 'Mark', last_name: 'Guillen' },
+        { first_name: 'KB', last_name: 'Tonel' }
+    ],
+    'Instructors': [
+        { first_name: 'Michael', last_name: 'Choi' },
+        { first_name: 'Martin', last_name: 'Puryear' }
+    ]
+};
+
+}
